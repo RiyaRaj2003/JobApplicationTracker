b7c3f1ae3a4d006bb558b4b0bfd23b7ad84539bb
RiyaRaj2003 <riya.rajifs@gmail.com>
2025-07-12 23:00:00 +0530

    commit changes in backend

--- JobApplicationTracker/src/main/java/com/riya/jobapplicationtracker/JobapplicationtrackerApplication.java
+++ JobApplicationTracker/src/main/java/com/riya/jobapplicationtracker/JobapplicationtrackerApplication.java
@@ -1,17 +1,16 @@
 package com.riya.jobapplicationtracker;
 
-import com.riya.jobapplicationtracker.model.JobApplication;
-import com.riya.jobapplicationtracker.repository.JobApplicationRepository;
-import org.springframework.boot.CommandLineRunner;
+// import com.riya.jobapplicationtracker.model.JobApplication;
+// import com.riya.jobapplicationtracker.repository.JobApplicationRepository;
+// import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
+// import org.springframework.context.annotation.Bean;
+// import java.time.LocalDate;
 
 @SpringBootApplication
 public class JobapplicationtrackerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JobapplicationtrackerApplication.class, args);
 	}
